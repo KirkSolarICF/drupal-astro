f3647401aad2a1748cf2d57d992b6cb0398f8524
28963_icfcorp <28963@icf.com>
2025-02-10 10:58:47 -0500

    Adding a few test pages

---
+++ src/api/json.ts
@@ -0,0 +1,45 @@
+import {Jsona} from "jsona";
+import {DrupalJsonApiParams} from "drupal-jsonapi-params";
+import type {DrupalNode} from "../types.ts";
+import type {TJsonApiBody} from "jsona/lib/JsonaTypes";
+
+// NOTE, required for localhost to connect to Drupal. node default connection is IPv6, this forces IPv4
+import dns from 'node:dns';
+dns.setDefaultResultOrder('ipv4first');
+
+// Get the Drupal Base Url.
+export const baseUrl: string = import.meta.env.DRUPAL_BASE_URL;
+
+/**
+ * Fetch url from Drupal.
+ *
+ * @param url
+ *
+ * @return Promise<TJsonaModel | TJsonaModel[]> as Promise<any>
+ */
+export const fetchUrl = async (url: string): Promise<any> => {
+    const request: Response = await fetch(url);
+    const json: string | TJsonApiBody = await request.json();
+    const dataFormatter: Jsona = new Jsona();
+    return dataFormatter.deserialize(json);
+}
+
+/**
+ * Get all published articles.
+ *
+ * @return Promise<DrupalNode[]>
+ */
+export const getPages= async (): Promise<DrupalNode[]> => {
+    const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+    params
+        .addFields('',  [
+            "title",
+            "path",
+            "body",
+            "created",
+        ])
+        .addFilter("status", "1");
+    const path: string = params.getQueryString();
+    return await fetchUrl(baseUrl + '/jsonapi/node/page?' + path);
+}
+
