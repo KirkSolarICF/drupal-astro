a52088471fc88cdaeb9dbad76bc7da50d5440c29
Kirk Solar <kirk.solar@icf.com>
2025-02-10 14:33:49 -0800

    updating image reference in article page template

--- src/api/drupal.ts
+++ src/api/drupal.ts
@@ -4,8 +4,8 @@ import type {DrupalNode} from "../types.ts";
 import type {TJsonApiBody} from "jsona/lib/JsonaTypes";
 
 // NOTE, required for localhost to connect to Drupal. node default connection is IPv6, this forces IPv4
-import dns from 'node:dns';
-dns.setDefaultResultOrder('ipv4first');
+// import dns from 'node:dns';
+// dns.setDefaultResultOrder('ipv4first');
 
 // Get the Drupal Base Url.
 export const baseUrl: string = import.meta.env.DRUPAL_BASE_URL;
@@ -30,15 +30,25 @@ export const fetchUrl = async (url: string): Promise<any> => {
  * @return Promise<DrupalNode[]>
  */
 export const getArticles = async (): Promise<DrupalNode[]> => {
+    // builds the query string parameters for an article
     const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+    // include any field you need to retrieve from the database within the "article" content type
     params
         .addFields("node--article", [
             "title",
             "path",
+            "field_media_image",
             "body",
             "created",
         ])
+        .addInclude(['field_media_image.field_media_image'])
+        .addFields('media--image', ['field_media_image'])
+        .addFields('file--file', ['uri', 'image_style_uri', 'resourceIdObjMedia'])
         .addFilter("status", "1");
+    // convert the params into a query string
     const path: string = params.getQueryString();
+    // append the params to build the database fetch, export the result to the article page template
     return await fetchUrl(baseUrl + '/jsonapi/node/article?' + path);
 }
+
+// NOTE, the addInclude() query function might be the way we get flex content. It looks like that is our reference proxy to "JOIN" our database query
