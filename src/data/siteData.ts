45521d53b4e557c6597405662986351590c99639
28963_icfcorp <28963@icf.com>
2025-02-12 13:54:27 -0500

    added a link component, working on adding a some more flexible layouts

---
+++ src/data/siteData.ts
@@ -0,0 +1,5 @@
+export const baseData = {
+  title: "RISE Homes",
+  description:
+    "RISE Homes (Rebuilding Incentives for Sustainable Electric Homes) is a program to provide financial incentives, assistance, and support for rebuilding energy-efficient, all-electric homes throughout the state of California.",
+};
